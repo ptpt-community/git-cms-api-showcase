409c5e06951a1bfd9fa346b3dde3535b572026d6
Labnan <khalidmasum@iut-dhaka.edu>
2022-04-19 05:14:43 +0600

    Implement CRUD

--- app.js
+++ app.js
@@ -9,11 +9,16 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(restrictOrigins);
 
 //Define the endpoint
 app.get("/pages/:page_name", getPage);
 app.get("/posts/:post_name", getPost);
+app.get("/posts/", getPosts);
+app.post("/post/create", createPost);
+app.post("/posts/:post_name/edit", editPost);
+app.get("/posts/:post_name/delete", deletePost);
 
 
 
@@ -53,15 +58,104 @@ async function respondFromBackend(backendRequestPayload, res) {
 }
 
 async function getPost(req, res) {
-    const pageName = req.params.post_name;
+    const postName = req.params.post_name;
     const backendRequestPayload = {
         action: "getEntry",
         branch: "main",
-        params: { branch: "main", path: `site/content/post/${pageName}.md` }
+        params: { branch: "main", path: `site/content/post/${postName}.md` }
     };
     await respondFromBackend(backendRequestPayload, res);
     return;
 
 };
 
+
+function createPost(req, res) {
+    const postName = req.body.name;
+    percistEntry(req, res, true, postName);
+}
+
+function editPost(req, res) {
+    const postName = req.params.post_name;
+    percistEntry(req, res, false, postName);
+}
+
+
+async function percistEntry(req, res, newEntry, postName) {
+
+
+    const commitMessage = newEntry ? `Create Post “${postName}” from API` : `Update Post “${postName}” from API`;
+    const backendRequestPayload = {
+        branch: "main",
+        action: "persistEntry",
+        params: {
+            branch: "main",
+            dataFiles: [
+                {
+                    path: `site/content/post/${postName}.md`,
+                    slug: postName,
+                    raw: processSiteBody(req.body)
+                }],
+            assets: [],
+            options: { newEntry, commitMessage, "collectionName": "post", "useWorkflow": false, "unpublished": false, "status": "draft" }
+        }
+    };
+    // res.json(backendRequestPayload);
+    await respondFromBackend(backendRequestPayload, res);
+
+    return;
+
+
+    function processSiteBody(body) {
+        if (body.image !== undefined) {
+            body.image = `\nimage: ${body.image}\n`;
+        }
+        return `---\ntitle: ${body.title}\ndescription: ${body.description}${body.image}---\n` + body.content;
+    }
+};
+
+
+
+async function getPosts(req, res) {
+
+    const backendRequestPayload = {
+        branch: "main",
+        action: "entriesByFolder",
+        params: {
+            branch: "main",
+            folder: "site/content/post",
+            extension: "md",
+            depth: 1
+        }
+    }
+
+    await respondFromBackend(backendRequestPayload, res);
+    return;
+
+
+}
+
+
+
+async function deletePost(req, res) {
+    const postName = req.params.post_name;
+    const backendRequestPayload = {
+        branch: "main",
+        action: "deleteFiles",
+        params: {
+            branch: "main",
+            paths: [
+                `site/content/post/${postName}.md`
+            ],
+            options: {
+                commitMessage: `Delete Post “${postName}”`
+            }
+        }
+    }
+
+    await respondFromBackend(backendRequestPayload, res);
+    return;
+
+
+}
 
